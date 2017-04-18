1b3a9c690d8830e4b416be442d34ba404a1fd7af
saurabhojha075 <ojhasaurabh1992@gmail.com>
2017-04-18 13:32:49 +0530

    salluss

--- app/src/main/java/com/example/administrator/maa/ItemDetailActivity.java
+++ app/src/main/java/com/example/administrator/maa/ItemDetailActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * An activity representing a single Item detail screen. This
@@ -85,6 +87,6 @@ public class ItemDetailActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
+        Log.d("hehehehehehehehe","sallusnz");
     }
 }
